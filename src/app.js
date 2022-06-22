16e5013aaa7df84a407408b0cac4137baafc725d
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-06-22 19:51:47 +0000

    avance ejercicio Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
+let quien = ["the dog", "my granma", "his turtle", "my bird"];
+let que = ["eat", "pissed", "crushed", "broked"];
+let cuando = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 window.onload = function() {
   //write your code here
+
   console.log("Hello Rigo from the console!");
 };
